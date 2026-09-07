88ae85286ef5a34e2cd938c917df61906b66381c
learningblueoceancapital-png <learning.blueoceancapital@gmail.com>
2026-09-07 12:55:12 +0530

    Update recipient list to lavneesh/param.parekh/kinjal.shah

--- chartink_4star_buy.py
+++ chartink_4star_buy.py
@@ -41,8 +41,8 @@ GMAIL_PASSWORD = os.getenv("GMAIL_APP_PASSWORD", "")
 
 EMAIL_TO = [
     "lavneesh@blueoceancapital.co.in",
-    "operations@blueoceancapital.co.in",
-    "research@blueoceancapital.co.in",
+    "param.parekh@blueoceancapital.co.in",
+    "kinjal.shah@blueoceancapital.co.in",
 ]
 
 SCREENER_URL  = "https://chartink.com/screener/4-star-buy"
