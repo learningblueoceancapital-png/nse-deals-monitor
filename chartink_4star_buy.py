5d9b1387f760769a1b9a7ec15ccb943b556cf7d6
learningblueoceancapital-png <learning.blueoceancapital@gmail.com>
2026-09-16 17:35:46 +0530

    Remove param.parekh from recipients

--- chartink_4star_buy.py
+++ chartink_4star_buy.py
@@ -41,7 +41,6 @@ GMAIL_PASSWORD = os.getenv("GMAIL_APP_PASSWORD", "")
 
 EMAIL_TO = [
     "lavneesh@blueoceancapital.co.in",
-    "param.parekh@blueoceancapital.co.in",
     "kinjal.shah@blueoceancapital.co.in",
 ]
 
